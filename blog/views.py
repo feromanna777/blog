1fab3f1be771b0445998a01ea2b2ab74f87e9bdf
feromanna777 <anakrinicnaa05@gmail.com>
2023-05-05 06:20:29 +0300

    asddas

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
 from django.views.generic import ListView, DetailView # new
 from .models import Post
+from django.views.generic.edit import CreateView, UpdateView
 class BlogListView(ListView):
  model = Post
  template_name = "home.html"
 class BlogDetailView(DetailView): # new
  model = Post
  template_name = "post_detail.html"
+
+class BlogCreateView(CreateView): # new
+ model = Post
+ template_name = "post_new.html"
+ fields = ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView): # new
+ model = Post
+ template_name = "post_edit.html"
+ fields = ["title", "body"]
+
 
 
